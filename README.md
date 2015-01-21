f2dc5ca3dd7a85080ab9de6df3792044b468961a
phil-arista <phil@arista.com>
2015-01-20 20:25:04 -0500

    Wrap install with python script

--- README.md
+++ README.md
@@ -3,16 +3,120 @@
 ##Introduction
 The guides below will help you quickly turn up vEOS nodes running on VMware (FusionPro/Workstation) or VirtualBox. The Packer configuration files (.json) define 4 VM nodes and assign specific virtual networks to switch interfaces to simulate isolated connections. You can setup a quick demo by using [packer-veos](https://github.com/arista-eosplus/packer-veos) and [packer-ztpserver](https://github.com/arista-eosplus/packer-ztpserver). Both of these installations use the [ztpserver-demo](https://github.com/arista-eosplus/ztpserver-demo) files to create a simple test environment.
 
-##Memory Requirements
+###Memory Requirements
 **Each vEOS node requires at least 2G of RAM and so does the ZTPServer VM.** While the memory for the ZTPServer could be reduced without impacting the performance much (500M RAM should also work just fine), the memory requirements for vEOS are not as flexible (1.5G per VM might work with some older vEOS instances, but it is NOT recommended to go lower than that). Hence, in order to run this demo it is recommended to use a machine which has at least 16G or RAM. If that is not possible, the demo can still be used, but the VMs should not all be started at the same time (the maximum number of VMs which could run in parallel will depend on memory size of the system, given the constraints from above).
 
-###VMware Virtual Network Setup
-![vEOS Networks](https://raw.githubusercontent.com/arista-eosplus/packer-veos/master/gh-pages/images/vEOS-spine-leaf-vmware.jpg)
+###What's Supported
+* **Host Operating Systems**
+  * Windows
+  * Mac Osx
+* **Host Hypervisors**
+  * VirtualBox
+  * VMware Fusion (Professional Required)
+  * VMware Workstation
 
-###VirtualBox Virtual Network Setup
-![vEOS Networks](https://raw.githubusercontent.com/arista-eosplus/packer-veos/master/gh-pages/images/vEOS-spine-leaf-vbox.jpg)
+###Prerequisites
+* You will need to log into your Arista.com account to obtain the following files from https://www.arista.com/en/support/software-download:
+  * Aboot-[release].iso and **rename it Aboot-vEOS.iso**
+  * vEOS-[release].vmdk and **rename it vEOS.vmdk**
+* Python (this has been tested on Python 2.7.x)
+  * [Get Python 2.7.x for Windows](https://www.python.org/downloads/windows/)
+* Git (technically optional but makes life easier)
+  * [Get Git](http://git-scm.com/downloads)
+* User running script has sudo privileges (NIX-based)
+
+> **NOTE:** You can use any Aboot and vEOS release combination you want, but make sure that the two are compatible. If you are not sure the two files you intend to use are compatible, please contact support@arista.com.
 
 ##Getting Started
+The [create-veos.py](https://github.com/arista-eosplus/packer-veos/blob/master/create-veos.py) helps you create a small spine/leaf topology that you can use to test ZTPServer.
 
- * [Create vEOS nodes in VMware](https://github.com/arista-eosplus/packer-veos/tree/master/VMware)
- * [Create vEOS nodes in VirtualBox](https://github.com/arista-eosplus/packer-veos/tree/master/VirtualBox)
+**Step 1.** Retrieve the packer-veos configuration files [here](https://github.com/arista-eosplus/packer-veos/archive/master.zip) or use:
+<pre>
+git clone https://github.com/arista-eosplus/packer-veos.git
+cd packer-veos
+</pre>
+
+**Step 2.** Place **vEOS.vmdk** and **Aboot-vEOS.iso** into the correct directories. Your directory should look like:
+
+```
+    packer-veos
+        /vmware
+            - vEOS.json
+            /source
+                - vEOS.vmx
+                - vEOS.vmdk
+                - Aboot-vEOS.iso
+        /virtualbox
+                - vEOS.json
+                - vEOS-windows.json
+            /source
+                - vEOS.ovf
+                - vEOS.vmdk
+                - Aboot-vEOS.iso
+```
+**Step 3.** Begin the build with the arguments of your choice
+<pre>
+arista:packer-veos arista$ ./create-veos.py --help
+usage: create-veos.py [-h]
+[-n {vEOS1,vEOS2,vEOS3,vEOS4} [{vEOS1,vEOS2,vEOS3,vEOS4} ...]]
+[-b BOOTTIME] [-N VMNAME]
+{vmware,virtualbox}
+
+Automatically install the vEOS Demo Nodes
+
+positional arguments:
+{vmware,virtualbox}   Hypervisor to create VM in
+
+optional arguments:
+-h, --help            show this help message and exit
+-n {vEOS1,vEOS2,vEOS3,vEOS4} ..., --nodes {vEOS1,vEOS2,vEOS3,vEOS4} ...
+Desired vEOS nodes to create or ignore to build all
+-b BOOTTIME, --boottime BOOTTIME
+This is the time Packer will wait before it sends
+commands over VNC
+-N VMNAME, --vmname VMNAME
+The Virtual Machine name prefix
+</pre>
+
+To build all of the vEOS nodes with VirtualBox:
+```
+arista:packer-veos arista$ ./create-veos.py virtualbox
+```
+
+To build all of the vEOS nodes with VMware:
+```
+arista:packer-veos arista$ ./create-veos.py vmware
+```
+
+To build specific nodes with VirtualBox:
+```
+arista:packer-veos arista$ ./create-veos.py virtualbox -n vEOS1 vEOS3
+```
+
+To build specific nodes with VirtualBox and lower the boot wait time:
+```
+arista:packer-veos arista$ ./create-veos.py virtualbox -n vEOS1 vEOS3 -b 1m30s
+```
+
+To build specific nodes with VirtualBox and define the VM name prefix:
+```
+arista:packer-veos arista$ ./create-veos.py virtualbox -n vEOS1 vEOS3 -N testName
+```
+
+###Post-Installation Tips
+####Set the PATH Variable
+If Packer is installed via the script above, the packer binary path wasn't permanently
+added to your system ```PATH``` variable.  If you intend on using Packer again, you might consider
+updating your ```PATH``` variable.
+
+**NIX-Based Terminal**
+<pre>
+echo "export PATH=$PATH:~/packer-bin" >> ~/.bash_profile
+</pre>
+and then restart your bash.
+
+**Windows-Based Command Prompt**
+<pre>
+setx PATH "%PATH%;%USERPROFILE%\packer-bin"
+</pre>
+and then restart your ```cmd.exe```.
---
+++ lib/eosplusvnets.py
@@ -0,0 +1,449 @@
+#!/usr/bin/python
+
+##############################################
+# Library for creating ZTPServer-related Demos
+# Author: eosplus-dev@arista.com
+# Date: 20150113
+##############################################
+
+import sys
+import os
+import re
+import platform
+import argparse
+import subprocess
+import datetime
+import urllib
+import zipfile
+import getpass
+
+
+packerURL = "http://dl.bintray.com/mitchellh/packer"
+packerVersion = "0.7.5"
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+def getHostOS():
+    return platform.system().lower()
+
+def getHostArch():
+    is_64bits = sys.maxsize > 2**32
+    return 64 if is_64bits else 32
+
+def find(path, name):
+    print "Searching %s for %s..." % (path, name)
+    # Look recursively through OS for files
+    for root, dirs, files in os.walk(path):
+        #print files
+        if name in files:
+            print "Found file here:%s" % root
+            return root
+        if name in dirs:
+            print "Found file here:%s" % root
+            return os.path.join(root, name)
+
+    # File wasn't found, return false
+    return False
+
+def getUnzipped(url, dest, fn):
+    name = os.path.join(dest, fn)
+    try:
+        if find(dest, fn.split(".zip")[0]):
+            print "%s already exists, no need to download again." % fn.split(".zip")[0]
+        else:
+            print "Downloading Packer binaries to %s" % name
+            print "This may take a few minutes (~85MB)..."
+            name, hdrs = urllib.urlretrieve(url, name)
+            print "Download successful!"
+    except IOError, e:
+        print "Can't retrieve %r to %r: %s" % (url, name, e)
+        raise
+
+    try:
+        print "Unzipping %s..." % name
+        with zipfile.ZipFile(name, "r") as z:
+            bin = os.path.join(dest, "packer-bin")
+            z.extractall(bin)
+    except zipfile.error, e:
+        print "Bad zipfile (from %r): %s" % (url, e)
+        raise
+    print "Unzipped successfully to %s" % bin
+    return bin
+
+def installPacker(hostOS, hostArch):
+    if hostArch == 64:
+        arch = "amd64"
+    else:
+        arch = "386"
+
+    url = "%s/packer_%s_%s_%s.zip" % (packerURL, packerVersion, hostOS, arch)
+
+    installPath = os.path.expanduser('~')
+    packerZipDir = getUnzipped(url, installPath, "packer-bin.zip")
+    packerDir = os.path.join(installPath, "packer-bin")
+
+    # Make all Packer binaries executable
+    for file in os.listdir(packerDir):
+        file = os.path.join(packerDir, file)
+        os.chmod(file, 0o777)
+
+    # Add packer-bin to path
+    os.environ["PATH"] += os.pathsep + packerDir
+    print "Updated path to be:%s" % os.environ["PATH"]
+    try:
+        print "Checking if install was successful by running 'packer -v'"
+        subprocess.call(["packer", "-v"])
+    except OSError as e:
+        if e.errno == os.errno.ENOENT:
+            print "Packer installation failed"
+            raise
+        else:
+            print "Something else went wrong"
+            raise
+
+    print "Packer installed!"
+
+def getActiveNets(cmd, regex):
+    # Get existing networks and return array of numbers
+    try:
+        ifconfig = subprocess.check_output(cmd)
+        return re.findall(r"%s" % regex, ifconfig)
+    except OSError as e:
+        if e.errno == os.errno.ENOENT:
+            print "vboxnet creation failed. Check output above"
+            raise
+        else:
+            print "Something else went wrong"
+            raise
+
+def createVBoxNets(hostOS, hostArch, libDir):
+    print "Creating virtual networks for Virtual Box"
+
+    if hostOS == "darwin":
+        # Open VirtualBox App
+        print "Opening VirtualBox application..."
+        cmd = ["open", "-a", "VirtualBox"]
+        process = subprocess.Popen(cmd)
+
+        #Get list of current networks
+        cmd = ["ifconfig", "-a"]
+        regex = "vboxnet(\d+)"
+        activeNets = getActiveNets(cmd, regex)
+
+        print "\nAnalyzing Host-Only Networks..."
+
+        # Create vmnets
+        vmnets = (0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+        if len(activeNets) < len(vmnets):
+            if len(activeNets) > 0:
+                print "Existing Host-Only networks found:"
+                for n in activeNets:
+                    print " - %s" % n
+            else:
+                print "No existing Host-Only networks found."
+
+            numCreate = len(vmnets) - len(activeNets)
+            for i in range(0, numCreate):
+                try:
+                    cmd = "%s/vboxmanage" % libDir
+                    subprocess.call([cmd, "hostonlyif", "create"])
+                except OSError as e:
+                    if e.errno == os.errno.ENOENT:
+                        print "vboxnet creation failed. Check output above"
+                        raise
+                    else:
+                        print "Something else went wrong"
+                        raise
+        else:
+            print "Enough existing virtual networks exist. Let's just reconfigure them."
+
+        try:
+            for net in vmnets:
+
+                print "Creating/modifying vboxnet%s" % net
+                network = int(net) + 128
+                print " - Assigning vboxnet%s to 172.16.%s.1/24" % (net, network)
+
+                cmd = "%s/vboxmanage" % libDir
+                vboxnet = "vboxnet%s" % net
+                ip = "172.16.%s.1" % network
+                subprocess.call([cmd, "hostonlyif", "ipconfig", vboxnet,
+                                 "-ip", ip, "-netmask", "255.255.255.0"])
+        except OSError as e:
+            if e.errno == os.errno.ENOENT:
+                print "vboxnet creation failed. Check output above"
+                raise
+            else:
+                print "Something else went wrong"
+                raise
+
+        # Remove any DHCP Servers from virtual networks
+        try:
+            cmd = "%s/vboxmanage" % libDir
+            dhcpList = subprocess.check_output([cmd, "list", "dhcpservers"])
+            regex = "NetworkName:\s+(\S+)"
+            hostOnlyDHCPSrvs = re.findall(r"%s" % regex, dhcpList)
+
+            print "Disabling DHCP Servers"
+            for srv in hostOnlyDHCPSrvs:
+                print "Disabling HostOnlyIf DHCP Server %s" % srv
+                subprocess.call([cmd, "dhcpserver", "remove", "--netname", srv])
+
+            return True
+
+        except OSError as e:
+            if e.errno == os.errno.ENOENT:
+                print "vboxnet creation failed. Check output above"
+                raise
+            else:
+                print "Something else went wrong"
+                raise
+
+    elif hostOS == "windows":
+        # Open VirtualBox App
+        print "Opening VirtualBox application..."
+        cmd = ["%s/VirtualBox.exe" % libDir]
+        process = subprocess.Popen(cmd)
+
+        #Get list of current networks
+        cmd = ["ipconfig"]
+        regex = "Ethernet.*(VirtualBox Host-Only.*):"
+        activeNets = getActiveNets(cmd, regex)
+
+        print "\nAnalyzing Host-Only Networks..."
+
+        # Create vmnets
+        vmnets = ("", " #2", " #3", " #4", " #5", " #6", " #7", " #8", " #9", " #10")
+        if len(activeNets) < len(vmnets):
+            if len(activeNets) > 0:
+                print "Existing Host-Only networks found:"
+                for n in activeNets:
+                    print " - %s" % n
+            else:
+                print "No existing Host-Only networks found."
+
+            numCreate = len(vmnets) - len(activeNets)
+            print "Creating %s new Host-Only Networks" % numCreate
+            for i in range(0, numCreate):
+                try:
+                    cmd = "%s/vboxmanage" % libDir
+                    subprocess.call([cmd, "hostonlyif", "create"])
+                except OSError as e:
+                    if e.errno == os.errno.ENOENT:
+                        print "vboxnet creation failed. Check output above"
+                        raise
+                    else:
+                        print "Something else went wrong"
+                        raise
+        else:
+            print "Enough existing virtual networks exist. Let's just reconfigure them."
+            print "Existing Host-Only networks found:"
+            for n in activeNets:
+                print " - %s" % n
+
+        try:
+            network = 128
+            for net in vmnets:
+
+                print "Modifying VirtualBox Host-Only Ethernet Adapter%s" % net
+                print " - Assigning VirtualBox Host-Only Ethernet Adapter%s to 172.16.%s.1/24\n" % (net, network)
+
+                cmd = "%s/vboxmanage" % libDir
+                vboxnet = "VirtualBox Host-Only Ethernet Adapter%s" % net
+                ip = "172.16.%s.1" % network
+                subprocess.call([cmd, "hostonlyif", "ipconfig", vboxnet,
+                                 "-ip", ip, "-netmask", "255.255.255.0"])
+                network += 1
+
+        except OSError as e:
+            if e.errno == os.errno.ENOENT:
+                print "vboxnet creation failed. Check output above"
+                raise
+            else:
+                print "Something else went wrong"
+                raise
+
+        # Remove any DHCP Servers from virtual networks
+        try:
+            cmd = "%s/vboxmanage" % libDir
+            dhcpList = subprocess.check_output([cmd, "list", "dhcpservers"])
+            regex = "NetworkName:\s+(\S+.*)"
+            hostOnlyDHCPSrvs = re.findall(r"%s" % regex, dhcpList)
+
+            print "Disabling DHCP Servers"
+            for srv in hostOnlyDHCPSrvs:
+                print " - Disabling DHCP Server %s" % srv
+                subprocess.call([cmd, "dhcpserver", "remove", "--netname", "%s" % srv], shell=True)
+        except OSError as e:
+            if e.errno == os.errno.ENOENT:
+                print "vboxnet creation failed. Check output above"
+                raise
+            else:
+                print "Something else went wrong"
+                raise
+        return True
+
+def createVmNets(hostOS, hostArch, libDir):
+    print "Creating virtual networks for VMware"
+
+    if hostOS == "darwin":
+        # Open VMware Fusion App
+        cmd = ["open", "-a", "VMware Fusion"]
+        process = subprocess.Popen(cmd)
+
+        #Get list of current networks
+        cmd = ["ifconfig"]
+        regex = "vmnet(\d+)"
+        activeNets = getActiveNets(cmd, regex)
+
+        print "\nAnalyzing Host-Only Networks..."
+
+        # Create/modify vmnets
+        vmnets = (2, 3, 4, 5, 6, 7, 9, 10, 11)
+        try:
+            if len(activeNets) > 0:
+                print "Existing vmnets found:"
+                for n in activeNets:
+                    print " - VMnet%s" % n
+            else:
+                print "No existing vmnets found."
+
+            print "Creating/modifying vmnets"
+            for net in vmnets:
+
+                print "Creating/modifying vmnet%s" % net
+                print "---------------------------"
+
+                network = int(net) + 128
+                netcfgCmd = r"%s/vmnet-cfgcli" % libDir
+                cfgCmd = r"%s/vmnet-cli" % libDir
+                dhcpCmd = "VNET_%s_DHCP" % net
+                subnetCmd = "VNET_%s_HOSTONLY_SUBNET" % net
+                subnet = "172.16.%s.0" % network
+                netmaskCmd = "VNET_%s_HOSTONLY_NETMASK" % net
+                virtualCmd = "VNET_%s_VIRTUAL_ADAPTER" % net
+                subprocess.call(["sudo", netcfgCmd, "vnetcfgadd", dhcpCmd, "no"])
+                subprocess.call(["sudo", netcfgCmd, "vnetcfgadd", subnetCmd, subnet])
+                subprocess.call(["sudo", netcfgCmd, "vnetcfgadd", netmaskCmd, "255.255.255.0"])
+                subprocess.call(["sudo", netcfgCmd, "vnetcfgadd", virtualCmd, "yes"])
+
+            # Configure and restart to take effect
+            print "Committing vmware network services"
+            print "----------------------------------"
+            subprocess.call(["sudo", cfgCmd, "--configure"])
+
+            print "Stopping vmware network services"
+            print "--------------------------------"
+            subprocess.call(["sudo", cfgCmd, "--stop"])
+
+            print "Starting vmware network services"
+            print "--------------------------------"
+            subprocess.call(["sudo", cfgCmd, "--start"])
+
+            print "VMNets Installed!"
+
+        except OSError as e:
+            if e.errno == os.errno.ENOENT:
+                print "vmnet creation failed. Check output above"
+                raise
+            else:
+                print "Something else went wrong"
+                raise
+
+        return True
+
+    elif hostOS == "windows":
+        # Open VMware Fusion App
+        cmd = ["%s/vmware.exe" % libDir]
+        process = subprocess.Popen(cmd)
+
+        #Get list of current networks
+        cmd = ["ipconfig"]
+        regex = "VMnet(\d+)"
+        activeNets = getActiveNets(cmd, regex)
+
+        print "\nAnalyzing Host-Only Networks..."
+
+        # Create/modify vmnets
+        vmnets = ["2", "3", "4", "5", "6", "7", "9", "10", "11"]
+
+        try:
+            if len(activeNets) > 0:
+                print "Existing vmnets found:"
+                for n in activeNets:
+                    print " - VMnet%s" % n
+            else:
+                print "No existing vmnets found."
+
+            # Trim vmnets
+            createNets = [x for x in vmnets if x not in activeNets]
+
+            netcfgCmd = r"%s/vnetlib.exe" % libDir
+            print netcfgCmd
+
+            # Stop Workstation services - nat dhcp
+            print "Stopping VMware Workstation NAT service"
+            rc = subprocess.call([netcfgCmd, "--", "stop", "nat"])
+            print "Stopping VMware Workstation DHCP service"
+            rc = subprocess.call([netcfgCmd, "--", "stop", "dhcp"])
+
+            # create networks that dont already exist
+            for net in createNets:
+                netName = "vmnet%s" % net
+                print " - Creating new virtual network %s" % netName
+                rc = subprocess.call([netcfgCmd, "--", "add",
+                                      "adapter", netName])
+                rc = subprocess.call([netcfgCmd, "--", "update",
+                                      "adapter", netName])
+
+            # Configure ALL of the networks in vmnets list
+            for net in vmnets:
+                network = 128 + int(net)
+                netName = "vmnet%s" % net
+                mask = "255.255.255.0"
+                addr = "172.16.%s.0" % network
+                print "Modifying virtual network %s" % netName
+                print " - setting netmask to %s" % mask
+                rc = subprocess.call([netcfgCmd, "--", "set", "vnet",
+                                      netName, "mask", mask])
+                print " - setting address to %s" % addr
+                rc = subprocess.call([netcfgCmd, "--", "set", "vnet",
+                                      netName, "addr", addr])
+                print " - disabling DHCP server on vmnet%s" % net
+                rc = subprocess.call([netcfgCmd, "--", "remove",
+                                      "dhcp", netName])
+                print " - disabling NAT on vmnet%s" % net
+                rc = subprocess.call([netcfgCmd, "--", "remove",
+                                      "nat", netName])
+                print " - saving changes for vmnet%s" % net
+                rc = subprocess.call([netcfgCmd, "--", "update",
+                                      "dhcp", netName])
+                rc = subprocess.call([netcfgCmd, "--", "update",
+                                      "nat", netName])
+                rc = subprocess.call([netcfgCmd, "--", "update",
+                                      "adapter", netName])
+
+            # Start DHCP and NAT
+            print "Starting VMware Workstation NAT service"
+            rc = subprocess.call([netcfgCmd, "--", "start", "nat"])
+            print "Starting VMware Workstation DHCP service"
+            rc = subprocess.call([netcfgCmd, "--", "start", "dhcp"])
+
+            print "VMNets Installed!"
+        except OSError as e:
+            if e.errno == os.errno.ENOENT:
+                print "vmnet creation failed. Check output above"
+                raise
+            else:
+                print "Something else went wrong"
+                raise
+
+        return True
